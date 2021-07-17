a90a0b91ac07bca838b458f1896888bffe42b88d
Caleb Maclennan <caleb@alerque.com>
2021-07-17 21:41:02 +0300

    Bootstrap packer

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,4 @@
+local execute = vim.api.nvim_command
 local cmd = vim.cmd
 local fn = vim.fn
 
@@ -5,3 +6,10 @@ local fmt = string.format
 
 -- Source legacy init.vim
 cmd(fmt("source %s/legacy_init.vim", fn.stdpath("config")))
+
+-- Bootstrap packer
+local install_path = fmt("%s/site/pack/packer/start/packer.nvim", fn.stdpath("data"))
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({"git", "clone", "https://github.com/wbthomason/packer.nvim", install_path})
+  execute("packadd packer.nvim")
+end
