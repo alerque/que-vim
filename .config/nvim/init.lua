4185888aa733341434b621b8240847b0a8740ae6
Caleb Maclennan <caleb@alerque.com>
2021-07-17 21:58:03 +0300

    Have packer take over and start managing itself

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,3 +13,5 @@ if fn.empty(fn.glob(install_path)) > 0 then
   fn.system({"git", "clone", "https://github.com/wbthomason/packer.nvim", install_path})
   execute("packadd packer.nvim")
 end
+
+local plugins = require("plugins")
