b7f48c820dfefb283b70bc7bd1e0dbcdb2106257
Caleb Maclennan <caleb@alerque.com>
2022-06-27 16:25:53 +0300

    Style plugins using table format so they can be extended with options

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,26 +1,27 @@
-return require("packer").startup(function()
-  use("wbthomason/packer.nvim")
+return require("packer").startup(function(use)
+  use { "wbthomason/packer.nvim" }
 
-  use("tpope/vim-repeat")
-  use("tpope/vim-sensible")
-  use("tpope/vim-eunuch")
-  use("tpope/vim-tbone")
-  use("tpope/vim-unimpaired")
-  use("tpope/vim-abolish")
-  use("tpope/vim-speeddating")
-  use("tpope/vim-characterize")
-  use("tpope/vim-endwise")
-  use("tpope/vim-fugitive")
-  use("tpope/vim-rhubarb")
+  use { "tpope/vim-repeat" }
+  use { "tpope/vim-sensible" }
+  use { "tpope/vim-eunuch" }
+  use { "tpope/vim-tbone" }
+  use { "tpope/vim-unimpaired" }
+  use { "tpope/vim-abolish" }
+  use { "tpope/vim-speeddating" }
+  use { "tpope/vim-characterize" }
+  use { "tpope/vim-endwise" }
+  use { "tpope/vim-fugitive" }
+  use { "tpope/vim-rhubarb" }
 
-  use("alerque/vim-surround") -- Forked from tpope to work with vim-textobj-quote
+  -- Forked from tpope to work with vim-textobj-quote
+  use { "alerque/vim-surround" }
 
-  use("nvim-treesitter/nvim-treesitter")
-  use("neovim/nvim-lspconfig")
+  use { "nvim-treesitter/nvim-treesitter" }
+  use { "neovim/nvim-lspconfig" }
 
-  use("norcalli/nvim-colorizer.lua")
+  use { "norcalli/nvim-colorizer.lua" }
 
-  use("github/copilot.vim")
+  use { "github/copilot.vim" }
 
 end)
 
