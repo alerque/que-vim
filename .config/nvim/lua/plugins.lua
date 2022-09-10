76259394842e69edb2b674d4a4735c5af4f1ab88
Caleb Maclennan <caleb@alerque.com>
2022-09-10 16:17:41 +0300

    Add and configure telescope similar to previous clap based setup

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,6 +1,52 @@
 return require("packer").startup(function(use)
   use { "wbthomason/packer.nvim" }
 
+  use { "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("nvim-web-devicons").setup {
+        default = true
+      }
+    end
+  }
+
+  -- required by telescope
+  use { "nvim-lua/plenary.nvim" }
+
+  use { "nvim-telescope/telescope-fzy-native.nvim" }
+  use { "nvim-lua/telescope.nvim",
+    branch = "0.1.x",
+    config = function ()
+      local telescope = require("telescope")
+      local builtin = require("telescope.builtin")
+      telescope.setup {
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-?>"] = "which_key",
+              ["<C-h>"] = "select_horizontal"
+            }
+          }
+        },
+      }
+      telescope.load_extension("fzy_native")
+      vim.keymap.set("n", "!!", builtin.builtin, { noremap = true })
+      vim.keymap.set("n", "!b", builtin.buffers, { noremap = true })
+      vim.keymap.set("n", "!c", builtin.commands, { noremap = true })
+      vim.keymap.set("n", "!f", builtin.find_files, { noremap = true })
+      vim.keymap.set("n", "!F", builtin.git_files, { noremap = true })
+      vim.keymap.set("n", "!g", builtin.live_grep, { noremap = true })
+      vim.keymap.set("v", "!g", builtin.grep_string, { noremap = true })
+      vim.keymap.set("n", "!h", builtin.command_history, { noremap = true })
+      vim.keymap.set("n", "!m", builtin.marks, { noremap = true })
+      vim.keymap.set("n", "!s", builtin.search_history, { noremap = true })
+      vim.keymap.set("n", "!t", builtin.tags, { noremap = true })
+      vim.keymap.set("n", "!p", builtin.registers, { noremap = true })
+      vim.keymap.set("n", "!q", builtin.quickfix, { noremap = true })
+      vim.keymap.set("n", "!z", builtin.spell_suggest, { noremap = true })
+      vim.keymap.set("v", "!z", builtin.spell_suggest, { noremap = true })
+    end
+  }
+
   use { "tpope/vim-repeat" }
   use { "tpope/vim-sensible" }
   use { "tpope/vim-eunuch" }
