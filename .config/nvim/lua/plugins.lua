5eed83ee743357f60499ca0e13caaf574860489e
Caleb Maclennan <caleb@alerque.com>
2022-09-11 00:19:04 +0300

    Copy some LSP bindings from somewhere

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -81,6 +81,23 @@ return require("packer").startup(function(use)
       local on_attach = function(_, bufnr)
         vim.o.omnifinc = "v:lua.vim.lsp.omnifunc"
         local opts = { noremap = true, silent = true }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set('n', '<leader>wl', function () print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
+        vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', '<leader>e', vim.lsp.diagnostic.show_line_diagnostics, opts)
+        vim.keymap.set('n', '[d', vim.lsp.diagnostic.goto_prev, opts)
+        vim.keymap.set('n', ']d', vim.lsp.diagnostic.goto_next, opts)
+        vim.keymap.set('n', '<leader>q', vim.lsp.diagnostic.set_loclist, opts)
+        vim.keymap.set('n', '<leader>so', require('telescope.builtin').lsp_document_symbols, opts)
         vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
       end
       local capabilities = vim.lsp.protocol.make_client_capabilities()
