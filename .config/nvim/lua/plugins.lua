aee7565ae4c19b3ec0b38c8a119f5e71e5c8ffd6
Caleb Maclennan <caleb@alerque.com>
2022-09-10 17:20:23 +0300

    Get startet with cmp completion

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -92,8 +92,33 @@ return require("packer").startup(function(use)
     end
   }
 
-  use {
-    "github/copilot.vim",
+  use { "hrsh7th/nvim-cmp",
+    config = function ()
+      local cmp = require("cmp")
+      cmp.setup {
+        mapping = {
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.close(),
+          ["<CR>"] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true
+          }
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "calc" },
+        }
+      }
+    end
+  }
+  use { "hrsh7th/cmp-calc" }
+  use { "hrsh7th/cmp-nvim-lsp" }
+
+  use { "github/copilot.vim",
     config = function ()
       vim.g.copilot_no_tab_map = true
       vim.g.coppilot_filetypes = {
