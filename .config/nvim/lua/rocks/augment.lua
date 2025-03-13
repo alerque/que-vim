75fb4fdfc4e5f443602d2969a59a4ea05146b9bb
Caleb Maclennan <caleb@alerque.com>
2025-03-13 19:24:33 +0300

    Deconflict key bindings for Augment

--- .config/nvim/lua/rocks/augment.lua
+++ .config/nvim/lua/rocks/augment.lua
@@ -1,3 +1,9 @@
+vim.g.augment_disable_tab_mapping = true
+vim.keymap.set("i", "<C-l>", "<Cmd>:call augment#Accept()<Cr>", {
+   noremap = true,
+   silent = true,
+})
+
 vim.g.augment_workspace_folders = {
    "~/projects/decasify",
    "~/projects/git-warp-time",
